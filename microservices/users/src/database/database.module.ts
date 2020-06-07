903a2d3dfcde71100efbe8e310a1fc57c0cd7881
Daniel Mejia <danielmejia@lmejia-mac.local>
2020-06-07 13:09:41 -0500

    UserDatabaseModule
    - Add the migration to create the users table.
    - Add the database module

---
+++ microservices/users/src/database/database.module.ts
@@ -0,0 +1,10 @@
+// Dependencies
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
+})
+export class DatabaseModule { }
